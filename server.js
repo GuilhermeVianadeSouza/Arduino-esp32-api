8de57faba92b0c6f5f0b06739c2da767b4f946c5
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2026-04-16 11:12:48 -0300

    realizado com sucesso

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+let dados = [];
+
+app.get("/sensor", (req, res) => {
+     const {temp, hum } = req.query;
+
+     if(!temp || !hum){
+        return res.status(400).send("Dados inválidos");
+     }
+
+     console.log(`Temp: ${temp} | Umidade: ${hum}`);
+
+     const leitura = {
+      temperatura: temp,
+      umidade: hum,
+      data: new Date()
+   };
+   
+      dados.push(leitura);
+   
+      console.log(leitura);
+
+     res.send("OK")
+});
+
+const PORT = process.env.PORT || 3000;
